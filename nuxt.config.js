312765a2d30e02f93085826dae10a34bf677a183
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2021-05-24 21:16:31 +0300

    Disabled live edit and try to change PrismJS theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,14 @@ export default {
   ],
 
   // Content module configuration (https://go.nuxtjs.dev/config-content)
-  content: {},
+  content: {
+    liveEdit: false,
+    markdown: {
+      prism: {
+        theme: 'prism-themes/themes/prism-xonokai.css'
+      }
+    }
+  },
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
