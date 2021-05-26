9a6a4bead6ec0af2dae806fffe0b916585731f77
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2021-05-26 19:42:24 +0300

    Removed, because caused errors "this file already importing". As file still not used, it was removed

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,6 @@ export default {
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
     theme: {
       dark: true,
       themes: {
