6e3704d2513368e339d4ff0940fcf9b96a0de3fa
NEK-RA <NEK-RA@users.noreply.github.com>
2021-06-23 14:18:44 +0000

    deploy: aa62d405c81fd0f7cf15c37de69e89f088fd91e6

--- _nuxt/static/1622047618/projects/q32/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622047618",layout:"default",error:a,state:{layout:{title:""}},serverRendered:true,routePath:"\u002Fprojects\u002Fq32",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"00c857f7"}}}}(null));
--- _nuxt/static/1622047618/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622047618",layout:"default",error:a,state:{layout:{title:""}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"00c857f7"}}}}(null,"\u002F"));
