8255a68c9dd02593d6d414d29125eec9e95c274f
NEK-RA <NEK-RA@users.noreply.github.com>
2021-08-26 18:24:21 +0000

    deploy: 15a2dfebeaee10d9d3390631d3b45f9f46638ed3

--- _nuxt/static/1624463397/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624463397",layout:"default",error:a,state:{layout:{title:""}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8301f120"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1630002255/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630002255",layout:"default",error:a,state:{layout:{title:""}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"83e9eaa8"}}}}(null,"\u002F"));
