b9f07b518aa3d37d85f02d78f137700b654cf455
NEK-RA <NEK-RA@users.noreply.github.com>
2021-10-09 16:47:05 +0000

    deploy: a17cab29cd8589033b72696932ebc4216c24773c

---
+++ _nuxt/static/1633798018/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633798018",layout:"default",error:a,state:{layout:{title:""}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7a0a1968"}}}}(null,"\u002F"));
