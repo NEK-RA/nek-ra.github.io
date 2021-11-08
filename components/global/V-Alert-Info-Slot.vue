b8ecb2fbdd4d3c94d7a95a453fd2380e447e591d
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2021-11-08 16:53:41 +0300

    V-Alert-Md was replaced with 3 slot components

---
+++ components/global/V-Alert-Info-Slot.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-alert
+    type="info"
+    outlined
+    prominent
+    text
+    dense
+  >
+    <slot>Forgot to add content here!</slot>
+  </v-alert>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ components/global/V-Alert-Warning-Slot.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-alert
+    type="warning"
+    outlined
+    prominent
+    text
+    dense
+  >
+    <slot>Forgot to add content here!</slot>
+  </v-alert>
+</template>
+
+<script>
+export default {
+
+}
+</script>
