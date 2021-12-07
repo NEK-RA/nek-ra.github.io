461317b6ef2e9b676bda3bea89ac47bda64ddeee
NEK-RA <NEK-RA@users.noreply.github.com>
2021-12-07 18:29:09 +0000

    deploy: 1f727f4e85734a873ea12528d4cd572b7923f5ed

--- _nuxt/static/1638440993/blog/appimage-packaging-experience/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638440993",layout:"default",error:a,state:{layout:{title:""}},serverRendered:true,routePath:"\u002Fblog\u002Fappimage-packaging-experience",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"64c87f3b"}}}}(null));
---
+++ _nuxt/static/1638901743/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638901743",layout:"default",error:a,state:{layout:{title:""}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"20c5bb75"}}}}(null,"\u002F"));
